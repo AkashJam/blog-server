7395345c773ad053fa0c0333d28a6259ea6ab91d
Akash James <akashaloysius.james@gmail.com>
2023-11-15 20:23:25 +0100

    Still trying to fix importing package in package

--- server/pkg/http/rest/handler.go
+++ server/pkg/http/rest/handler.go
@@ -1,9 +1,10 @@
 package rest
 
 import (
+	. "github.com/AkashJam/blog-server/server/pkg/adding"
 	"github.com/gin-gonic/gin"
 )
 
 func ArticleRoutes(g *gin.RouterGroup) {
-	g.GET("/", adding.getArticles)
+	g.GET("/", getArticles)
 }
